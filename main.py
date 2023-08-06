8b3b274f02dc6400b00f3634e79b37410286061b
Jonathan Helms <jonathanhelms5@gmail.com>
2023-08-06 17:58:00 -0400

    update main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from pgan import run_gan
+
+# path_to_images = 'VanGogh/'
+# path_to_images = 'landscape/'
+# path_to_images = 'abstract/'
+path_to_images = 'ports/'
+
+
+def main():
+    run_gan(path_to_images)
+
+if __name__=="__main__": 
+    main()
